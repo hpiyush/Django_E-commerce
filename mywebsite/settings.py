c91fe54bbf206722f02a8f92c5bf99aa6bbbe16c
Cho <huntpiyush46@gmail.com>
2019-11-26 14:35:43 +0530

    Updated commit-b

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,9 +137,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 #
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'huntpiyush46@gmail.com'
-# EMAIL_HOST_PASSWORD = '10345quantumentanglement'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'huntpiyush46@gmail.com'
+EMAIL_HOST_PASSWORD = 'kwvn icnd evyy aefh'
+
+django_heroku.settings(local())
