f459bcae268e7968c09434925bf88762bf0bf204
Cho <huntpiyush46@gmail.com>
2019-11-29 18:04:41 +0530

    Updated commit-day5

--- cart/views.py
+++ cart/views.py
@@ -105,6 +105,7 @@ def all_orders(request, *args, **kwargs):
     orders = Order.objects.filter(owner=request.user, is_ordered=False)
     all_items = [item.items.all() for item in orders]
     make_list = [[i, j] for i in orders for j in all_items[0]]
+    req = ""
     if request.is_ajax():
         if request.method == "POST":
             req = ('raw data body: "%s"' %request.body)
