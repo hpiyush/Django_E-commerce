81bf41b1e46448a94785491d58982e6f5b9acd56
Cho <huntpiyush46@gmail.com>
2019-11-29 18:02:13 +0530

    Updated commit-day5

--- cart/views.py
+++ cart/views.py
@@ -105,12 +105,15 @@ def all_orders(request, *args, **kwargs):
     orders = Order.objects.filter(owner=request.user, is_ordered=False)
     all_items = [item.items.all() for item in orders]
     make_list = [[i, j] for i in orders for j in all_items[0]]
+    if request.is_ajax():
+        if request.method == "POST":
+            req = ('raw data body: "%s"' %request.body)
 
     context = {
         'make_list': make_list,
         'args': args,
         'kwargs': kwargs,
-        'request': request,
+        'request': req,
     }
     return render(request, 'user/orders.html', context)
 
